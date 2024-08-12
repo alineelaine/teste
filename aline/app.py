097784f303b4ec51c476ccb271ea91bdb369ddfd
alineelaine <121210728+alineelaine@users.noreply.github.com>
2024-08-12 00:49:55 -0300

    a

--- aline/app.py
+++ aline/app.py
@@ -5,13 +5,6 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-def load_data():
-    global df_alcadas, df_email, df_comite, df_transferencias
-    df_alcadas = pd.read_excel('controleAlcadas.xlsx', sheet_name='alcadas')
-    df_transferencias = pd.read_excel('controleTransf.xlsx', sheet_name='transferencias')
-    df_email = pd.read_excel('controleEmail.xlsx', sheet_name='email')
-    df_comite = pd.read_excel('controleComite.xlsx', sheet_name='comite')
-
 
 @app.route('/')
 def dados():
@@ -29,6 +22,8 @@ def dados():
 
     resumo_alcadas_table = resumo_alcadas.to_html(index=False, classes='table table-striped')
 
+
+
     # Dados de Transferências
     df_historico_transf = pd.read_excel('historico.xlsx', sheet_name='HistTransf')
     df_historico_transf['DATA'] = pd.to_datetime(df_historico_transf['DATA'], format='%d/%m/%Y')
@@ -43,11 +38,41 @@ def dados():
 
     resumo_transf_table = resumo_transf.to_html(index=False, classes='table table-striped')
 
-    # Dados para o gráfico
-    alcadas_por_tempo_resposta = df_historico_alcadas.groupby('TempoResposta').size().reset_index(name='Quantidade')
+    return render_template('dados.html', resumo_alcadas_table=resumo_alcadas_table, resumo_transf_table=resumo_transf_table)
 
-    # Passar ambas as tabelas e os dados do gráfico para o template
-    return render_template('dados.html', resumo_alcadas_table=resumo_alcadas_table, resumo_transf_table=resumo_transf_table, alcadas_por_tempo_resposta=alcadas_por_tempo_resposta.to_dict(orient='list'))
+@app.route('/dados_grafico')
+def dados_grafico():
+    df_historico_alcadas = pd.read_excel('historico.xlsx', sheet_name='HistAlcadas')
+    df_historico_alcadas['DATA'] = pd.to_datetime(df_historico_alcadas['DATA'], format='%d/%m/%Y')
+    df_historico_alcadas['Mês'] = df_historico_alcadas['DATA'].dt.to_period('M')
+
+    resumo_alcadas = df_historico_alcadas.groupby('Mês').agg(
+        Solicitações=('DATA', 'count')
+    ).reset_index()
+
+    dados = {
+        'meses': resumo_alcadas['Mês'].astype(str).tolist(),
+        'solicitacoes': resumo_alcadas['Solicitações'].tolist()
+    }
+
+    return jsonify(dados)
+
+@app.route('/dados_grafico_transf')
+def dados_grafico_transf():
+    df_historico_transf = pd.read_excel('historico.xlsx', sheet_name='HistTransf')
+    df_historico_transf['DATA'] = pd.to_datetime(df_historico_transf['DATA'], format='%d/%m/%Y')
+    df_historico_transf['Mês'] = df_historico_transf['DATA'].dt.to_period('M')
+
+    resumo_transf = df_historico_transf.groupby('Mês').agg(
+        Solicitações=('DATA', 'count')
+    ).reset_index()
+
+    dados = {
+        'meses': resumo_transf['Mês'].astype(str).tolist(),
+        'solicitacoes': resumo_transf['Solicitações'].tolist()
+    }
+
+    return jsonify(dados)
 
 
 if __name__ == '__main__':
